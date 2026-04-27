9443e3b6a57121d02f305eaf7ba7aafeb8e91a3a
Tarun Kumar <tarunbindal0001@gmail.com>
2026-04-27 15:51:39 +0530

    Implemented incremental loading for fact_orders using order_time filter

--- models/gold/warehouse/fact_orders.sql
+++ models/gold/warehouse/fact_orders.sql
@@ -1,3 +1,8 @@
+{{ config(
+    materialized='incremental',
+    unique_key='order_id'
+) }}
+
 select
     order_id,
     user_id,
@@ -6,4 +11,9 @@ select
     delivery_minutes,
     status,
     order_time
+
 from {{ ref('silver_orders') }}
+
+{% if is_incremental() %}
+where order_time > (select max(order_time) from {{ this }})
+{% endif %}
